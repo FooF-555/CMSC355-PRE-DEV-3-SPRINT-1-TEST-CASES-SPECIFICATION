48bb348c97c993cfd7ba5dc3f718fc831ce63ecd
Kevin Nguyen <nguyenkm9@vcu.edu>
2026-03-28 22:34:36 -0400

    Update DeviceManagement.java

--- DeviceManagement.java
+++ DeviceManagement.java
@@ -1,7 +1,32 @@
 import java.util.*;
 
 public class DeviceManagement {
-    ArrayList<IoTDevice> = new ArrayList<>;
-    Scanner userInput = new Scanner(System.in);
+    private ArrayList<IoTDevice> deviceList;
 
+    // Device Functions
+    public void createDeviceList() {
+        deviceList = new ArrayList<>();
+    }
+
+    public void addDevice(IoTDevice newDevice) {
+        deviceList.add(newDevice);
+    }
+
+    public void removeDevice(IoTDevice inputDevice) {
+        deviceList.remove(inputDevice);
+    }
+
+    // For setting a device's attributes
+    public void setName(String newName) {
+        
+    }
+
+    public void displayMenu() {
+        System.out.println("/////// Welcome to the IoT Device Management Page!" +
+        "// Please select one of the options (1-4) below: " +
+        "1) Add Device\n" +
+        "2) Remove Device\n" +
+        "3) Set Device\n" +
+        "4) Cancel\n");
+    }
 }
