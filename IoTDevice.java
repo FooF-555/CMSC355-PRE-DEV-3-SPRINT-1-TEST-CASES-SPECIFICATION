51d177df4a49f617827ac845e0e3c8890e83b335
Kevin Nguyen <nguyenkm9@vcu.edu>
2026-03-28 22:05:52 -0400

    Create IoTDevice.java

---
+++ IoTDevice.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class IoTDevice {
+
+    // Device fields
+    private final int deviceID;
+    private String deviceName;
+    private final String manufacturer;
+    private final int modelNumber;
+    private String status;
+    private String firmwareVersion;
+
+    // Constructor
+    public IoTDevice(int deviceID, String deviceName, String manufacturer, int modelNumber, String status, String firmwareVersion) {
+        this.deviceID = deviceID;
+        this.deviceName = deviceName;
+        this.manufacturer = manufacturer;
+        this.modelNumber = modelNumber;
+        this.status = status;
+        this.firmwareVersion = firmwareVersion;
+    }
+
+    // Getter Methods
+    public int getDeviceID() {
+        return deviceID;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getVersion() {
+        return firmwareVersion;
+    }
+
+    // Setter Methods
+    public void setName(String newName) {
+        deviceName = newName;
+    }
+
+    public void setStatus(String newStatus) {
+        status = newStatus;
+    }
+
+    public void setVersion(String newVersion) {
+        firmwareVersion = newVersion;
+    }
+
+
+    // Get Device Details
+    public void getDeviceDetails() {
+        System.out.println("//// Details of Device: " + deviceName +
+        "- ID: " + deviceID +
+        "- Manufacturer: " + manufacturer +
+        "- Model Number: " + modelNumber +
+        "- Current Status: " + status +
+        "- Firmware Version: " + firmwareVersion);
+    }
+}
