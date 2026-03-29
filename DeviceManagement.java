cc7dc66d8de952927d4c24291c2581c698c9094b
Kevin Nguyen <nguyenkm9@vcu.edu>
2026-03-28 23:06:33 -0400

    Update DeviceManagement.java

--- DeviceManagement.java
+++ DeviceManagement.java
@@ -16,12 +16,56 @@ public class DeviceManagement {
         deviceList.remove(inputDevice);
     }
 
-    public void displayMenu() {
+    public static void displayMenu() {
         System.out.println("/////// Welcome to the IoT Device Management Page!" +
         "// Please select one of the options (1-4) below: " +
         "1) Add Device\n" +
         "2) Remove Device\n" +
         "3) Set Device\n" +
         "4) Cancel\n");
+    }
+
+    public void displayDeviceList() {
+        System.out.println("/////// Devices: ");
+        for (int i = 1; i < deviceList.size() + 1; i++) {
+            System.out.println("- ");
+            System.out.print(deviceList.get(i).getDeviceName());
+        }
+    }
+
+
+    public void initiateDeviceManager() {
+        Scanner input = new Scanner(System.in);
+
+        displayMenu();
+        
+        int userChoice = Integer.parseInt(input.nextLine());
+
+        int terminated = 0;
+
+        while (terminated != 1) {
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Please input the name of your IoT Device: ");
+                    int givenDeviceName = Integer.parseInt(input.nextLine());
+
+                    System.out.println("Please input the manufacturer of your IoT Device: ");
+                    String givenManufacturer = input.nextLine();
+
+                    
+
+                case 2:
+                    displayDeviceList();
+                    System.out.println("What device would you like to remove? (select number): ");
+                    int deviceChoice = Integer.parseInt(input.nextLine());
+                    deviceList.remove(deviceChoice);
+                case 3:
+                case 4:
+                    System.out.println("Goodbye!");
+                    terminated = 1;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
     }
 }
