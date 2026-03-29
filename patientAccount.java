86f2d834da2d98b2f6a581111f5b69d26f75359b
Kevin Nguyen <nguyenkm9@vcu.edu>
2026-03-28 20:17:23 -0400

    Update patientAccount.java

--- patientAccount.java
+++ patientAccount.java
@@ -1,5 +1,81 @@
-//patient variant of account
+import java.util.*;
 
-public class patientAccount extends account{
+public class PatientAccount {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private int age;
+    private String gender;
+    private String dateOfBirth; // In the form of MM/DD/YYYY
+    private int phoneNumber;
+    private String homeAddress;
+    private final int userID;
 
+    public PatientAccount(String firstName, String lastName, String password, String email, int age, String gender, String dateOfBirth, int phoneNumber, String homeAddress, int userID) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.homeAddress = homeAddress;
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getAccountDetails() {
+        return "//// Account Details of Patient: " + userID +
+        "\n// Name: " + firstName + " " + lastName +
+        "\n// Email Address: " + email +
+        "\n// Age: " + age +
+        "\n// Gender: " + gender +
+        "\n// Date of Birth: " + dateOfBirth +
+        "\n// Phone Number: " + phoneNumber +
+        "\n// Home Address: " + homeAddress + "\n";
+    }
 }
