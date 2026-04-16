c1c72a84df522ff660a82f94013bfe0dff448ef2
Kevin Nguyen <nguyenkm9@vcu.edu>
2026-04-16 00:54:04 -0400

    Added Spring Boot Controller

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,37 +1,14 @@
 package com.example.demo;
 
-import com.example.demo.mongoclasses.PatientAccount;
-import com.example.demo.mongoclasses.PatientAccountRepository;
-import com.example.demo.mongoclasses.ProviderAccount;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
-public class Demo1Application implements CommandLineRunner{
-
-    @Autowired
-    PatientAccountRepository patientRepo;
+public class Demo1Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(Demo1Application.class, args);
     }
 
-    @Override
-    public void run(String... args){
-        smth();
-    }
-
-    public void smth(){
-        patientRepo.save(new PatientAccount("first", "last", "p", "d@mail.com",
-                99, "g", "f", 2, "home", 123));
-        PatientAccount patient = patientRepo.findAccByName("first");
-
-        ProviderAccount doc = new ProviderAccount("f", "l", "p", "g@email.com", 9, "g", "f", 2
-        , "hose", 456);
-    }
 }
+
